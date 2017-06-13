c42f9a4bec80f800de8cc200beb1115554af188d
Orban Botond <orbanbotond@gmail.com>
2017-06-13 17:03:54 +0300

    Initial version

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ShopifyProductSlider
+  class Application < Rails::Application
+    config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
+    config.action_dispatch.default_headers.delete('X-Frame-Options')
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.application_name = 'My Product Slideshow Shopify App'
+  config.api_key = 'a110c144da10f17938dc25b3e2c6723e'
+  config.secret = '0ebb925469e8f6ba52a49986ada2e5f7'
+  config.scope = 'read_products'
+  config.embedded_app = true
+end
